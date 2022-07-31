2639d0af54ceed0c1c353ae1b2b4501c1057002a
Parnika2222 <parnikapandey74@gmail.com>
2022-07-31 14:49:28 +0530

    commit addfile

---
+++ Add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Add
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int sum1=sc.nextInt();
+        int sum2=sc.nextInt();
+
+        int res=sum1+sum2;
+        System.out.println(res);
+    }
+}
